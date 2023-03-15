cda92bf30221cb5884a2fea787b80ea7dc6aa869
= <jul204040204625@gmail.com>
2023-03-14 21:43:44 -0400

    create_db.sh

---
+++ create_db.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cat create_db.sql | sqlite3 database.sqlite3
---
+++ create_db.sql
@@ -0,0 +1,6 @@
+create table monday (name TEXT, desc TEXT, hosts TEXT, poster TEXT, discord TEXT, start_time INTEGER PRIMARY KEY, end_time INTEGER, is_running INTEGER);
+create table tuesday (name TEXT, desc TEXT, hosts TEXT, poster TEXT, discord TEXT, start_time INTEGER PRIMARY KEY, end_time INTEGER, is_running INTEGER);
+create table wednesday (name TEXT, desc TEXT, hosts TEXT, poster TEXT, discord TEXT, start_time INTEGER PRIMARY KEY, end_time INTEGER, is_running INTEGER);
+create table thursday (name TEXT, desc TEXT, hosts TEXT, poster TEXT, discord TEXT, start_time INTEGER PRIMARY KEY, end_time INTEGER, is_running INTEGER);
+create table friday (name TEXT, desc TEXT, hosts TEXT, poster TEXT, discord TEXT, start_time INTEGER PRIMARY KEY, end_time INTEGER, is_running INTEGER);
+
